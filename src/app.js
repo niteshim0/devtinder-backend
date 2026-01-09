b2995a531802ab0f8324b3ba8b805eabdaabc966
niteshim0 <niteshim111@gmail.com>
2026-01-09 14:09:35 +0530

    multipleRouteHandler

--- src/app.js
+++ src/app.js
@@ -3,6 +3,140 @@ const express = require("express");
 
 const app = express();
 
+// app.use('/users',()=>{
+//   console.log("This is going to be timed out because I am listening the client request but not sending any response(connection gets timed out)");
+// })
+
+// Case II : multiple routeHandlers but request-response cycle ends at Ist only
+// app.use('/users',
+//   (req,res)=>{
+//   console.log("Route Handler 1");
+//   res.send('Response from Route Handler 1');
+//   },
+//   (req,res)=>{
+//     console.log('Route Handler 2');
+//     res.send('Response from Route Handler 2')
+//   }
+// )
+
+// Case III : Two Route Handler , and second one is giving response not the first one this time , moving to next routeHandler using next() middleware
+// Ans resides how the recursion works(fuction calling function works) + runtime stack
+
+// app.use('/users',
+//   (req,res,next)=>{
+//   console.log("Route Handler 1");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 2');
+//     res.send('Response from Route Handler 2')
+//   }
+// )
+
+// Case IV : Two Route Handler , and second one is giving response not the first one this time despite first also having response, moving to next routeHandler using next() middleware , it will look like perfect code(because behaves how we have thought on client side) but on server side it gives error
+
+// app.use('/users',
+//   (req,res,next)=>{
+//   console.log("Route Handler 1");
+//   next();
+//   res.send('Response from Route Handler 1')
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 2');
+//     res.send('Response from Route Handler 2')
+//   }
+// )
+
+/* Case V : Multiple level of routeHandler , but response is sent back from only last level
+Ans :: Working of next() + Javascript is synchronous + request-response cycle working
+*/
+
+// app.use('/users',
+//   (req,res,next)=>{
+//   console.log("Route Handler 1");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 2');
+//     next();
+//   },
+//   (req,res,next)=>{
+//   console.log("Route Handler 3");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 4');
+//     next();
+//   },
+//   (req,res,next)=>{
+//   console.log("Route Handler 5");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 6');
+//     res.send('Response from Route Handler 6')
+//   }
+// )
+
+// Case VI :: route Handler gets stored inside arrays(works as normal case)
+// app.use('/users',
+//   [(req,res,next)=>{
+//   console.log("Route Handler 1");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 2');
+//     next();
+//   },
+//   (req,res,next)=>{
+//   console.log("Route Handler 3");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 4');
+//     next();
+//   },
+//   (req,res,next)=>{
+//   console.log("Route Handler 5");
+//   next();
+//   },
+//   (req,res,next)=>{
+//     console.log('Route Handler 6');
+//     res.send('Response from Route Handler 6(array ones)')
+//   }]
+// )
+
+// Case VII : Even at nth Level, no any response sent back but control gets transferred
+// It thinks next() is supposed to get,post(respective http Methods) , if get -> cannot GET error , if post cannot postError (it doesn't go into infinite loop , it is very different from not sending response back)
+app.use('/users',
+  (req,res,next)=>{
+  console.log("Route Handler 1");
+  next();
+  },
+  (req,res,next)=>{
+    console.log('Route Handler 2');
+    next();
+  },
+  (req,res,next)=>{
+  console.log("Route Handler 3");
+  next();
+  },
+  (req,res,next)=>{
+    console.log('Route Handler 4');
+    next();
+  },
+  [
+  (req,res,next)=>{
+  console.log("Route Handler 5");
+  next();
+  },
+  (req,res,next)=>{
+    console.log('Route Handler 6');
+    // No response even from last level but control gets transfered to next
+    next();
+  }]
+)
+
 
 app.get("/user/:userId/:name/:password",(req,res)=>{
   console.log(req.params);
