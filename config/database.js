fddf9956396d5d7f68d5c0e6597c7e938b3417e6
Nitesh Kushwaha <niteshim111@gmail.com>
2026-01-18 12:18:54 +0530

    Remove database connection code

--- config/database.js
+++ config/database.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
 
-const connectDB = async () => {   
-await mongoose.connect("mongodb+srv://niteshim0_db_user:7TqSPGaxDptI8xLk@nodejsseason1.plin1zo.mongodb.net/devTinder");
-};
-
-
-module.exports = {connectDB}
