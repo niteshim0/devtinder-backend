a544b43465e4b5c6a37c8ba57b766f37e27e9215
niteshim0 <niteshim111@gmail.com>
2026-01-18 12:21:18 +0530

    remove db connection from github

--- config/database.js
+++ config/database.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const connectDB = async () => {   
+await mongoose.connect("mongodb+srv://niteshim0_db_user:7TqSPGaxDptI8xLk@nodejsseason1.plin1zo.mongodb.net/devTinder");
+};
+
+
+module.exports = {connectDB}
