57d7946bb164887545204abb778bb54be028d54f
Nitesh Kushwaha <niteshim111@gmail.com>
2026-01-18 12:22:55 +0530

    Update MongoDB connection string to placeholder
    Replaced the MongoDB connection string with a placeholder.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {   
-await mongoose.connect("mongodb+srv://niteshim0_db_user:7TqSPGaxDptI8xLk@nodejsseason1.plin1zo.mongodb.net/devTinder");
+await mongoose.connect("mongodb_connection_string");
 };
 
 
