a62d4f29c5af0ce4fc7f944ae0525cb0c7233958
niteshim0 <niteshim111@gmail.com>
2026-02-19 12:21:02 +0530

    feed api

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = express.Router();
 const { ConnectionRequest } = require('../models/connectionRequest');
 const { userAuth } = require('../middlewares/auth');
+const { User } = require('../models/user');
 const USER_SAFE_DATA = "name bio skills experienceLevel location githubUrl linkedinUrl";
 
 userRouter.get(
@@ -49,7 +50,6 @@ userRouter.get(
       .populate("senderId",USER_SAFE_DATA)
       .populate("receiverId",USER_SAFE_DATA)
 
-      console.log(connectionRequest);
 
       const refinedData = connectionRequest.map((connection) => {
          if(connection.senderId.equals(loggedInUser._id)){
@@ -72,6 +72,57 @@ userRouter.get(
       })
     }
   }
+)
+.get("/user/display/feed",
+ userAuth,
+ async(req,res) => {
+  try {
+    const loggedInUser = req.user;
+    console.log(loggedInUser)
+  // read about comparison and logical operators in mongodb
+  // set data structure in JS
+  const connectionRequest = await ConnectionRequest.find({
+        $or : [
+          {senderId : loggedInUser._id },
+          {receiverId : loggedInUser._id}
+        ]
+  }).select("senderId receiverId");
+
+
+  const hideUsersFromFeed = new Set();
+
+  connectionRequest.forEach((req)=>{
+    hideUsersFromFeed.add(req.senderId.toString());
+    hideUsersFromFeed.add(req.receiverId.toString());
+  });
+  
+  // read about nesting of comparison and logical operators from Operators
+  // in MongoDB Docs --> logic similar to SQL but its easy than SQL
+
+  // displays only those people , who are not interested , ignored , accepted , rejected 
+  // either by me or them
+  const displayUsers = await User.find({
+    $and : [
+      {_id : {$nin : Array.from(hideUsersFromFeed)}},
+      {_id : {$ne : loggedInUser._id}}
+    ]
+  });
+  
+  return res.status(200).send({
+    success : true,
+    message : "Feed of loggedInUser",
+    displayUsers
+  })
+
+
+  } catch (error) {
+    return res.status(500).json({
+      success : false,
+      message : "Internal Server Error"
+    })
+  }
+  
+ }
 )
 
 
